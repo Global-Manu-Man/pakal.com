0f67e2190003b86ffbc161bd2b275e1ab46f5192
Muhammad Rafi <88550428+RaxonRafi@users.noreply.github.com>
2023-11-03 20:06:45 +0600

    Img compress, Navpan animation fix

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -604,7 +604,6 @@ function validate(email) {
 }
 
 $(window).load(function() {
-
     var tl = new TimelineMax({
         paused: true
       }),
@@ -614,15 +613,15 @@ $(window).load(function() {
       intro = new TimelineMax();
   
     intro
-      .from('.phone', 1, {
+      .from('.phone', 0.1, {
         autoAlpha: 0
       })
-      .from('.hello', 0.5, {
+      .from('.hello', 0.2, {
         autoAlpha: 0
-      }, 0.5)
+      }, 0.2);
   
     tl
-      .to('.hello', 0.3, {
+      .to('.hello', 0.1, {
         autoAlpha: 0
       })
       .set('.home', {
@@ -630,18 +629,18 @@ $(window).load(function() {
       })
       .set('.item', {
         scale: 1
-      }) // fix for a bug when on of the item was appearing at 0.5 scale
+      })
       .to('.home', 0.1, {
         scale: 1.2,
         yoyo: true,
         repeat: 1
       })
-      .to('.home', 0.3, {
+      .to('.home', 0.2, {
         x: 20,
         y: 20,
         ease: Elastic.easeOut.config(1, 0.5)
       }, 0)
-      .staggerFrom('.item', 0.7, {
+      .staggerFrom('.item', 0.3, {
         left: 20,
         top: 20,
         autoAlpha: 0,
@@ -653,21 +652,21 @@ $(window).load(function() {
       .set('.home', {
         className: '-=active'
       })
-      .staggerTo('.item', 0.7, {
+      .staggerTo('.item', 0.3, {
         left: 20,
         top: 20,
         autoAlpha: 0,
         scale: 0.5,
         ease: Elastic.easeOut.config(1, 0.5)
       }, 0.1)
-      .to('.hello', 0.3, {
+      .to('.hello', 0.2, {
         autoAlpha: 1
       })
-      .to('.home', 0.3, {
+      .to('.home', 0.2, {
         x: 0,
         y: 0,
         ease: Power2.easeOut
-      }, 0.5);
+      }, 0.2);
   
     $(document).on('click', '.home:not(.active)', function(e) {
       event.preventDefault();
@@ -685,5 +684,4 @@ $(window).load(function() {
         }
       });
     });
-  
   });
